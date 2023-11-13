391b0c22da5e9e1a365e07cafa52226f493636d4
LucasLopesFrazao <lucaslopesfrazao2003@gmail.com>
2023-11-13 20:13:54 -0300

    :bug: trying fix tailwind bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Login from "@/componentes/login";
-import TelaInicial from "@/componentes/tela-cadastro-geral";
+import TelaInicial from "@/components/tela-cadastro-geral";
 
 export default function Home() {
   return (
-    Login()
+    <TelaInicial />
   );
     
 }
--- src/components/tela-cadastro-geral/index.tsx
+++ src/components/tela-cadastro-geral/index.tsx
@@ -3,23 +3,17 @@ import Button from "../buttons";
 
 export default function TelaInicial() {
   return (
-    <div className="min-h-screen w-40 flex items-center justify-center bg-gray-100 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
       <div className=" w-full max-w-xs  space-y-8 bg-white p-6 rounded-xl shadow-md">
-        <div className="w-full flex items-center justify-center bg-red-300">
+        <div className="w-full flex items-center justify-center">
           <BsRocketTakeoffFill
             style={{ color: "#C5C5C5", width: "10rem", height: "10rem" }}
           />
         </div>
-        <div className="mt-4 space-y-8 ">
-          {Button({
-            texto: "Sensor",
-          })}
-          {Button({
-            texto: "Foguete",
-          })}
-          {Button({
-            texto: "Lançamento",
-          })}
+        <div className="mt-4 space-y-8 flex flex-col">
+          <Button texto="SENSOR"/>
+          <Button texto="FOGUETE"/>
+          <Button texto="LANÇAMENTO"/>
         </div>
       </div>
     </div>
