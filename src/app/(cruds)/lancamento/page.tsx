4f59a376eceff1c90c34f9d7902296df0e3f4bf2
PedroSiq <pedroaugustossiqueira@gmail.com>
2023-12-04 04:34:38 -0300

    create txt button

--- src/app/(cruds)/lancamento/page.tsx
+++ src/app/(cruds)/lancamento/page.tsx
@@ -10,6 +10,7 @@ import ListLaunch from "./listLaunch";
 import { Launch } from "@/app/types/crud";
 
 import { formatDate } from "@/app/utils/functions";
+import { Box, Modal, Typography } from "@mui/material";
 
 export default function Lancamento() {
   const [launch, setLaunch] = useState<Launch>({
@@ -310,6 +311,24 @@ export default function Lancamento() {
           <Button variant="primary" onClick={resetLaunch}>
             Limpar
           </Button>
+          <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
         </div>
       </div>
       <div className="w-full ">
